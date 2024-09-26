756a4ee6fb8654de8b38bee057e3bf44ddffca53
desahusjp <desarrollo.sistemas@hospitalsanjose.gov.co>
2024-09-26 14:28:04 -0500

    Create ApiGatewayServiceRequest.java
    Servicios que ofrece el api gateway

---
+++ src/main/java/husjp/api/mesaprocesos/request/ApiGatewayServiceRequest.java
@@ -0,0 +1,23 @@
+package husjp.api.mesaprocesos.request;
+
+import husjp.api.mesaprocesos.service.dto.AuthenticationResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(
+        name = "solutionv2Gateway",
+        value = "solutionv2Gateway",
+        path = "/"
+        //url = "${solution.api.gateway.url}"
+)
+public interface ApiGatewayServiceRequest {
+
+    @PostMapping(value = "excel/export-excel")
+    byte[] exportExcel(@RequestBody Object data);
+
+    @PostMapping("auth/login-microservices")
+    AuthenticationResponse loginMicroservices(@RequestBody Object request);
+
+}
